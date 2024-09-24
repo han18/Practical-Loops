c08f79e835c783a7182b42d47f4a408c1394a0fa
han18 <han.fadah@gmail.com>
2024-09-24 11:03:40 -0400

    working on part 2

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 // Part 1: Fizz Buzz
 // Loop through all numbers from 1 to 100.
 // If a number is divisible by 3, log “Fizz.”
@@ -10,15 +8,47 @@
 const num = 100;
 
 for (let i = 0; i <= num; i++) {
-    if (i % 3 == 0) {
-    console.log('Fizz')
-} else if (i % 5 == 0) {
-    console.log("Buzz")
-} else if (i % 3 == 0 && i % 5 == 0) {
-    console.log("Fizz Buzzzzzzzzzzzz")
-} else if (i % 3 !== 0 && i % 5 !== 0) {
-    console.log(`${i} not divisible by 3 or 5 `)
+  if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else if (i % 3 == 0 && i % 5 == 0) {
+    console.log("Fizz Buzzzzzzzzzzzz");
+  } else if (i % 3 !== 0 && i % 5 !== 0) {
+    console.log(`${i} not divisible by 3 or 5 `);
+  }
 }
 
+// Part 2: Prime Time
+
+// Declare an arbitrary number, n.
+// Create a loop that searches for the next prime number, starting at n and incrementing from there.
+// As soon as you find the prime number, log that number and exit the loop.
+
+// Switch would be the correct loop for this case
+
+for (let n = 0; n <= num; n++)  {
+    if(n == 4) {
+        console.log(5)
+    } else if (n == 5) {
+        console.log(7)
+    }
 }
 
+
+
+// for (let n = 0; n <= numPrime; n++) {
+//       switch (n) {
+//         case n % 5 == 1:
+//         console.log("this is a prime");
+//         break;
+//       }
+// }
+
+// for (let n = 0; n <= numPrime; n++) {
+//     if (n % 5 === 1) {
+//         console.log(`Prime ${5}`)
+//     }
+//     if ()
+
+// }
