c2f04aadbd6303a875e29bb8f5da169f83b29190
han18 <han.fadah@gmail.com>
2024-09-24 13:38:33 -0400

    creating the loops

--- main.js
+++ main.js
@@ -27,15 +27,6 @@ for (let i = 0; i <= num; i++) {
 
 // Switch would be the correct loop for this case
 
-for (let n = 0; n <= num; n++)  {
-    if(n == 4) {
-        console.log(5)
-    } else if (n == 5) {
-        console.log(7)
-    }
-}
-
-
 
 // for (let n = 0; n <= numPrime; n++) {
 //       switch (n) {
@@ -52,3 +43,40 @@ for (let n = 0; n <= num; n++)  {
 //     if ()
 
 // }
+
+
+
+for (let n = 5; n <= 100; n++) {
+    let numPrime = true;
+    console.log(n); 
+  
+    for (let i = 2; i < n; i++) {
+      if (n % i == 0) {
+        numPrime = false; 
+        break;
+      }
+    }
+  
+    if (numPrime) {
+      console.log(`${n} is a prime number.`);
+    }
+  }
+
+  // Part 3: Feeling Loopy
+
+  let stringCVS =
+  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+  console.log(stringCVS);
+
+  // will put the string in an array
+  let splitCVS = stringCVS.split("\n")
+
+  console.log(splitCVS);
+
+  // looping through each row using split method to create an array
+for (let i = 0; i < splitCVS.length; i++) {
+    // splitting the row into cells
+    const cells = splitCVS[i].split(","); // removing the comma 
+    console.log(cells[0], cells[1], cells[2], cells[3]);
+}
