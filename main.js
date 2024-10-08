c97622e847cd06525e8ee9016dcaee17d3529c01
han18 <han.fadah@gmail.com>
2024-10-08 14:15:00 -0400

    fixed the error in the code and looped through stringcvs

--- main.js
+++ main.js
@@ -87,8 +87,47 @@ for (let i = 0; i < stringCVS.length; i++) {
     const rowSplit = stringCVS[i]; 
 }
 
+// CSV data
+const csvData1 = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+const csvData2 = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
 
-// I got the code from Charles since it wasn't easy
+// looping through 
+for (const csvData of [csvData1, csvData2]) {
+    let row = [];
+    let cell = '';
+
+    for (let i = 0; i < csvData.length; i++) {
+        const char = csvData[i];
+
+        if (char === ',') {
+            // next cell
+            row.push(cell);
+            cell = ''; // next input
+        } else if (char === '\n' || (char === '\r' && csvData[i + 1] === '\n')) {
+            // the next row
+            row.push(cell); // Pushing the row
+            console.log(...row); 
+            row = []; // reseting
+            cell = ''; // reset next input
+
+            if (char === '\r') {
+                i++; //skipping the next character
+            }
+        } else {
+            cell += char;
+        }
+    }
+
+    // last cell if there's no new line at the end
+    if (cell) {
+        row.push(cell);
+        console.log(...row);
+    }
+}
+
+
+
+
 // Part 5
 // let returnToCSV = "";
 // returnToCSV += Object.keys(objArray[0]).toString() + "\n";
